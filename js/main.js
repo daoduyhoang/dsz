835540c13d3df57375f79930464109427e3c02fd
Hoang <daoduyhoang1994@gmail.com>
2022-05-18 00:07:10 +0700

    fix page 1

--- js/main.js
+++ js/main.js
@@ -13,5 +13,27 @@ $(document).ready(function(){
         prevArrow:"<button type='button' class='slick-prev pull-left'><img src='../images/page_3/circle-arow-left.svg'></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><img src='../images/page_3/circle-arow-right.svg'></i></button>"
     });
+
+    //THE MEET TEAM
+    let member = {
+        name:"THUỶ WOLF",
+        position:"Film Director –Producer",
+        exp:"Over 10 years of experience in the Television and Advertising industry",
+        study:"Bachelor of Art in Film Directing Academy of Theatre & Film –Ho Chi Minh City"
+    }
+    $('.item-teams').click(function(){
+        member.name = $(this).attr('data-name');
+        member.position = $(this).attr('data-position');
+        member.exp = $(this).attr('data-ex');
+        member.study = $(this).attr('data-study');
+        renderHtmlMeetTeam(member);
+    })
+
+    function renderHtmlMeetTeam(data){
+        $('.name-member').html(data.name);
+        $('.position-member').html(data.position);
+        $('.exp-member').html(data.exp);
+        $('.study-member').html(data.study);
+    }
   });
       
