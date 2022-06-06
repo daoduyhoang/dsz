cdc48ae9545129c871608fe481e85f8ac2a4a8d0
Hoang <daoduyhoang1994@gmail.com>
2022-06-06 21:58:06 +0700

    new animation

--- js/main.js
+++ js/main.js
@@ -15,13 +15,56 @@ $(document).ready(function(){
     });
 
 
-    $('#slide-global').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        autoplay:true,
-    });
+    // $('#slide-global').slick({
+    //     infinite: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     autoplay:true,
+    //     speed:1500,
+    // });
+    function animationSlide(callback1, callback2){
+        callback1();
+        callback2();
+    }
+
+   
+    function moveLeft(){
+            setTimeout(function(){
+                $(".content_l").animate({
+                    left: '-5%',
+                    opacity: '0',
+                });
+                $(".content_c").animate({
+                    // left: '-5%',
+                    opacity: '0',
+                });
+                $(".content_r").animate({
+                    right: '-5%',
+                    opacity: '0',
+                });
+        }, 1500);
+    }
+
+    function moveRight(){
+            setTimeout(function(){
+                $(".content_l").animate({
+                    left: '0',
+                    opacity: '1',
+                });
+                $(".content_c").animate({
+                    left: '50%',
+                    opacity: '1',
+                });
+                $(".content_r").animate({
+                    right:'0%',
+                    opacity: '1',
+                });
+        }, 2500);
+    }
+    setInterval(function(){
+        animationSlide(moveLeft,moveRight);
+    },8000)
 
     //THE MEET TEAM
     let member = {
